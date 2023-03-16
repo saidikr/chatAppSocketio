7695ef6464ad0d97c258dd007c6c81cfdd9e6691
said ikrouberkane <said.ikrouberkane@gmail.com>
2023-03-16 16:49:44 +0100

    deployed

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const instance=axios.create({
-    baseURL: 'http://localhost:3555'
-    //baseURL: 'https://chatmessengerapp1007.onrender.com/'
-})
+const instance = axios.create({
+ //baseURL: "http://localhost:3555",
+ baseURL: 'https://chatapp1007.onrender.com'
+});
 
 export default instance
